5eb2c9f8de0662cebd45cb1deade380232e85fbe
kilmas <kilmas@qq.com>
2020-08-22 12:49:05 +0800

    feat: first commit

---
+++ bs-config.js
@@ -0,0 +1,16 @@
+var proxy = require('http-proxy-middleware');
+var fomoProxy = proxy(['/**', '!**.js', '!**.css'], {
+    target: 'http://localhost:3000',
+    forward: 'http://localhost:3000/'
+});
+
+module.exports = {
+    server: {
+        baseDir: [
+            "./"
+        ]
+    }/*,
+    middleware: [
+        fomoProxy
+    ]*/
+};
